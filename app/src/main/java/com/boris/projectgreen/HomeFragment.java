ef4d57ef0c96647c82b56ada201c652d97cd8772
NicoUni99997 <29081997.Nl>
2021-06-17 16:36:25 +0200

    Update to Donazioni

--- app/src/main/java/com/boris/projectgreen/HomeFragment.java
+++ app/src/main/java/com/boris/projectgreen/HomeFragment.java
@@ -1,23 +1,17 @@
 package com.boris.projectgreen;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewbinding.ViewBinding;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.boris.projectgreen.databinding.ItemCustomFixedSizeLayout3Binding;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
