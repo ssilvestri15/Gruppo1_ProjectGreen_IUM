870ddfdbff5f871be7dbd02792dc5793920fbf54
NicoUni99997 <29081997.Nl>
2021-06-18 15:31:06 +0200

    Update to Registrazioni

--- app/src/main/java/com/boris/projectgreen/LoginActivity.java
+++ app/src/main/java/com/boris/projectgreen/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.boris.projectgreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void openFragmentRegistrazione(View view) {
+        FragmentManager fm = getSupportFragmentManager();
+        getSupportFragmentManager().beginTransaction();
     }
 }
